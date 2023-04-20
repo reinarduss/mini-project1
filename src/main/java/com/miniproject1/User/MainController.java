04cec3a5b02f289ee26b58442b3cae799f65bf1b
Christian Meidyanto <christianmeidyanto214@gmail.com>
2023-04-20 08:03:57 +0700

    get all user and getByUsername

--- src/main/java/com/miniproject1/User/MainController.java
+++ src/main/java/com/miniproject1/User/MainController.java
@@ -3,7 +3,9 @@ package com.miniproject1.User;
 // import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +17,11 @@ public class MainController {
     public Iterable<User> index() {
         return userRepo.findAll();
     }
+
+    @GetMapping("/login/{username}")
+    public ResponseEntity<String> getUserName(@PathVariable("username") String username) {
+        return ResponseEntity.of(userRepo.findById(username).map(User::getUsername));
+    }
+    
+
 }
